d7ef5dff9a8ad5502fae30a0b7a8c0459255528c
natasha pistarelli <natu123pistarelli@gmail.com>
2026-04-12 22:57:21 -0300

    prueba animator - todavia no funciona

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -4,15 +4,24 @@ public class MovimientoJugador : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Vector2 movement;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        // Evita diagonales
+        // evitar diagonales
         if (movement.x != 0)
             movement.y = 0;
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
     }
 
     void FixedUpdate()
